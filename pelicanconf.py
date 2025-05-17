8a5b0eeb678e545449698ba595801ab4968113b3
Pito Salas <pitosalas@users.noreply.github.com>
2025-05-17 11:04:29 -0400

    update

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,27 +3,35 @@ import os
 AUTHOR = 'Pito Salas'
 SITENAME = 'Salas Blog'
 SITETITLE = 'Salas Blog'
-SITEURL = 'https://pitosalas.github.io/salasblog'
+SITEURL = 'https://pitosalas.github.io/salasblog'  # Update to '' for local dev
 
 PATH = 'content'
 TIMEZONE = 'America/New_York'
 DEFAULT_LANG = 'en'
 DEFAULT_DATE_FORMAT = '%Y-%m-%d'
-
-ARTICLE_PATHS = ['posts']
-STATIC_PATHS = ['images', 'theme/css', 'theme/js', 'theme/fonts']
-
-THEME = 'notmyidea'
 DEFAULT_PAGINATION = 5
 
-# Enable page generation
+# Theme and Static Files
+THEME = 'notmyidea'
+STATIC_PATHS = ['images', 'theme/css', 'theme/js', 'theme/fonts']
+
+# Content Paths
+ARTICLE_PATHS = ['posts']
 PAGE_PATHS = ['pages', 'curacao', 'links', 'about', 'brandeis']
 
-# # Add menu items manually
-# MENUITEMS = [
-#     ('Home', '/'),
-#     ('Curacao', '/curacao.html'),
-#     ('Links', '/links.html'),
-#     ('About', '/about.html'),
-#     ('Brandeis', '/brandeis.html'),
-# ]
+# Menu Items (shown in top nav bar)
+MENUITEMS = [
+    ('Home', '/'),
+    ('Curacao', '/curacao.html'),
+    ('Brandeis', '/brandeis.html'),
+    ('Submit Post', 'https://salasblogf.fly.dev/')  # 👈 Link to your FastAPI form
+]
+
+# Feed generation (usually disabled during development)
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+AUTHOR_FEED_ATOM = 'feeds/{slug}.atom.xml'
+AUTHOR_FEED_RSS = 'feeds/{slug}.rss.xml'
+
+# Optional metadata
+RELATIVE_URLS = True  # Set to False in production (overridden by publishconf.py)
