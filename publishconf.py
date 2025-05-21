2ef29d6034c16e8f6b2258bcd4effc1e6cde924f
Pito Salas <pitosalas@users.noreply.github.com>
2025-05-21 09:48:24 -0400

    update

--- publishconf.py
+++ publishconf.py
@@ -4,8 +4,6 @@ sys.path.insert(0, os.path.dirname(__file__))  # keeps relative imports working
 
 import pelicanconf
 
-# Convert the relative THEME path to absolute
-pelicanconf.THEME = os.path.abspath(os.path.join(os.path.dirname(__file__), 'theme/notmyidea'))
-
+THEME = os.path.abspath(os.path.join(os.path.dirname(__file__), 'theme', 'notmyidea'))
 SITEURL = 'https://pitosalas.github.io/salasblog'
 RELATIVE_URLS = False
